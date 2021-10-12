5f9235b5503c91b206bfedd80ba83c550575c01e
Amado <amadodamis@gmail.com>
2021-10-12 11:18:06 -0300

    1/2 actualizacion sobre los ccss, integrado de ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const app = express ()
 
 const path = require ("path")
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.resolve(__dirname, "./public")));
@@ -9,22 +10,22 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 app.listen (3032, () => console.log ("Servidor corriendo http://localhost:3032/index" ,"http://192.168.0.145:3030/index" ));
 
 app.get("/index", (req,res) => {
-    const fileToSend = path.join (__dirname, "/views/index.html")
-    res.sendFile(fileToSend);
+    //const fileToSend = path.join (__dirname, "/views/index.html")
+    res.render("index.ejs");
 })
 
 app.get("/register", (req, res) => {
-    const fileToSend2 = path.join (__dirname, "/views/register.html");
-    res.sendFile(fileToSend2)
+    //const fileToSend2 = path.join (__dirname, "/views/register.html");
+    res.render("register.ejs")
 });
 
 app.get("/productCart", (req, res) => {
-    const fileToSend2 = path.join (__dirname, "/views/productCart.html");
-    res.sendFile(fileToSend2)
+    //const fileToSend2 = path.join (__dirname, "/views/productCart.html");
+    res.render("productCart.ejs")
 });
 app.get("/productDetail", (req, res) => {
-    const fileToSend2 = path.join (__dirname, "/views/productDetail.html");
-    res.sendFile(fileToSend2)
+   //const fileToSend2 = path.join (__dirname, "/views/productDetail.html");
+    res.render("productDetail")
 });
 
 app.post("/register", (req,res) => {
@@ -32,8 +33,8 @@ app.post("/register", (req,res) => {
 })
 
 app.get("/login", (req,res) => {
-    const fileToSend3 = path.join (__dirname, "/views/login.html");
-    res.sendFile(fileToSend3)
+    //const fileToSend3 = path.join (__dirname, "/views/login.html");
+    res.render("login.ejs")
 });
 
 app.post("/login", (req,res) => {
