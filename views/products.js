8c934736633231b536ade3586d33a2ca43c5158e
Amado <amadodamis@gmail.com>
2021-10-15 11:53:33 -0300

    users+products estructura

---
+++ views/products.js
@@ -0,0 +1,69 @@
+let tipoProducto=[
+  /*1 */"Almacenamiento",  
+  /*2*/ "Auriculare",       
+  /*3*/ "Cable",            
+  /*4*/ "Cooler",           
+  /*5*/ "Fuente",           
+  /*6*/ "Gabinete",
+  /*7*/ "Memoria Ram",
+  /*8*/ "Microfono",
+  /*9*/ "Monitor o televisor",
+  /*10*/"Motherboard",
+  /*11*/"Mouse",
+  /*12*/"Parlante",
+  /*13*/"Placa de video",
+  /*14*/"Sillas gammer",
+  /*15*/"Teclado",
+  /*16*/"Webcam",
+  /*17*/"Otro"
+]
+
+
+//producto rapido
+/*
+
+  {
+        Nombreproducto:"",
+        tipoProducto:"",                    //Tipo producto
+        img:"",
+        precio:"",                           //tipo num 
+        stock:true,
+        stockCant:"",                        //tipo num 
+        oferta:false,
+        ofertaPorcentaje:"",
+        PrecioConOferta:"",                 //tipo num 
+        cuotasSinInteres:false,
+    },
+    
+*/
+
+let productos=[
+    {
+        Nombreproducto:"",
+        tipoProducto:"",     //Tipo producto
+        img:"",
+        precio:"",      //tipo num 
+        stock:true,
+        stockCant:"",  //tipo num 
+        oferta:false,
+        ofertaPorcentaje:"",
+        PrecioConOferta:"", //tipo num 
+        cuotasSinInteres:false,
+    },
+    {
+        Nombreproducto:"",
+        tipoProducto:"",     //Tipo producto
+        img:"",
+        precio:"",      //tipo num 
+        stock:true,
+        stockCant:"",  //tipo num 
+        oferta:false,
+        ofertaPorcentaje:"",
+        PrecioConOferta:"", //tipo num 
+        cuotasSinInteres:false,
+    },
+
+
+]
+
+
---
+++ views/users.js
@@ -0,0 +1,25 @@
+let users=[
+    {
+        user:"jmilei",
+        nombre_y_apellido:"Javier Gerardo Milei",
+        mail:"jmilei@gmail.com",
+        password:"libertadAvanza2020",
+        domicilio:"calle falsa 123",
+    },
+    
+    {  user:"Dtrump2025",
+    nombre_y_apellido:"Donald trump",
+    mail:"dtrump@gmail.com",
+    password:"CHAINA",
+    domicilio:"725 5th Ave, New York, NY 10022, EE. UU.",
+    },
+
+    {
+        user:"Satoshi nakamoto ",
+        nombre_y_apellido:"Satoshi nakamoto",
+        mail:"SatoshiBTC@gmail.com",
+        password:"BTCISNOTABUBBLE",
+        domicilio:"NADIE LO SABE Y NADIE LO SABRA",
+    },
+
+]
