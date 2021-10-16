e3923c11cf45ed10dbb8f53c87c6074db9849e72
NK18OR <axel.leonelromano@gmail.com>
2021-10-16 15:12:56 -0300

    correcion errores en productCart y productDatail

--- routes/productCartRouter.js
+++ routes/productCartRouter.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var controller=require("../controllers/productCartController")
+var controller=require("../controllers/productCartController");
 
 
 router.get("", controller.productCart);
