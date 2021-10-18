0dc9f2e3f118a70fd8bbd97f602aa75d91bf9d9a
Amado <amadodamis@gmail.com>
2021-10-18 17:45:30 -0300

    edit product

--- controllers/uploadProductsController.js
+++ controllers/uploadProductsController.js
@@ -1,10 +1,16 @@
+let productos=require("../views/source/products");
+
 
 let controller = {
     upload: (req,res)=>{
-        res.render("uploadProducts")
+        res.render("uploadProducts",{productos:productos})
     },
     formulario: (req,res)=>{
         res.redirect("/")
+    },
+    edit:(req,res)=>{
+        let producto=req.params.productoId;
+        res.render("editProduct",{prod:productos[producto]})
     }
 }
 
--- routes/uploadProductsRouter.js
+++ routes/uploadProductsRouter.js
@@ -7,4 +7,7 @@ var controller=require("../controllers/uploadProductsController")
 router.get("", controller.upload);
 router.post("",controller.formulario)
 
+router.get("/editProduct/:productoId",controller.edit);
+//router.put("/editProduct",controller.edit);
+
 module.exports = router;
