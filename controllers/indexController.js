6d3be12f5af1f45a923dc131bf05ad90de9e979e
franciscocgce <franciscocgce@hotmail.com.ar>
2021-10-23 14:04:57 -0300

    empezando a usar la base de datos de los productos en formato JSON en lugar de la base de datos que teníamos en JS, para sprints futuros

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-/* En la constante "products" ya tienen los productos que están 
-guardados en la carpeta Data como Json (un array de objetos literales). Por ahora estamos tomando
+/* En la constante "productos" ya tienen los productos que están 
+guardados en la carpeta Data como Json (un array de objetos literales). Por ahora estábamos tomando
 los productos de la variable "productos" que está en JS, vamos a tener que empezar a utilizar la variable
-products de ahora en más me parece, para poder guardar los nuevos productos creados */
+productos que está en la base de datos JSON de ahora en más me parece, para poder guardar lonuevos productos creados */
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
