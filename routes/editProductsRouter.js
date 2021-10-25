9ea28e612624ad0474d894d3c05f62f377f71daf
franciscocgce <franciscocgce@hotmail.com.ar>
2021-10-25 17:11:57 -0300

    dentro del formulario de edición (editProduct), cambio los tags EJS del Placeholder al Value. Así nos evitamos  que cambiar los campos a mano siempre que hagamos algún update en el producto a editar

--- routes/editProductsRouter.js
+++ routes/editProductsRouter.js
@@ -5,6 +5,6 @@ var controller=require("../controllers/editProductsController")
 
 // Editar un Producto
 router.get("/:id",controller.edit);
-//router.put("/editProduct",controller.edit);
+//router.put("/",controller.edit);
 
 module.exports = router;
