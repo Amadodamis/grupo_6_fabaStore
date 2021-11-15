78b18916d65aded5922face160a3ba69dd34240b
Amado <amadodamis@gmail.com>
2021-11-15 15:29:09 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // ************ Require's ************
 const express = require ("express");
 const path = require ("path")
-const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
+
+// Para poder usar los métodos PUT y DELETE
+const methodOverride = require('method-override'); 
 
 // ************ express() - (don't touch) ************
 const app = express ();
@@ -15,6 +17,10 @@ app.use(methodOverride('_method')); // Para poder usar los métodos PUT y DELETE
 // ************ Template Engine - (NO TOCAR) ************
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+
+/***************** Cookies ************************/
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 //*************** Session******************************* */
 const session = require('express-session');
@@ -34,6 +40,7 @@ var registerRouter = require('./routes/registerRouter');
 var uploadProductsRouter = require ('./routes/uploadProductsRouter');
 var productsRouter = require('./routes/productsRouter'); // Rutas /products
 var editProductsRouter = require('./routes/editProductsRouter'); // Rutas /editProducts
+
 
 //llamados
 
