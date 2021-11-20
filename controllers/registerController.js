3ce8b1265a23049eb5b96a5aacc1ee0da2ea87f9
franciscocgce <franciscocgce@hotmail.com.ar>
2021-11-20 10:15:46 -0300

    modificando la base de datos de usuarios y preparando el controlador de registro para agregar nuevos usuarios

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,5 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 const { validationResult } = require('express-validator');
 
+/* En la constante "usuarios" ya tienen los usuarios que están JSON */
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+let tipoUsuario={
+    id: "",
+    modelo:"",
+    marca:"",
+    tipoProducto:"",
+    img:"",
+    precio:"",
+    stock:"",
+    stockCant:"",
+    oferta:"",
+    ofertaPorcentaje:"",
+    precioConOferta:"",
+    cuotasSinInteres:"",
+    cantCuotas: "",
+    interescuota: "",
+    precioEnCuotas: "",
+    especificaciones:""
+    }
 
 let controller={
 
@@ -12,6 +36,19 @@ let controller={
         }
     },
     formulario:(req,res)=>{
+
+        //requiero la informacion del formulario y la guardo en form
+        let form=req.body;
+
+        //requiero los datos de la foto que cargue desde el navegador.
+        let file=req.file;
+
+        //Guardo el valor del ultimo numero de producto
+        let id=usuarios.length+1;
+
+        //especifico el tipo de dato que va a ser
+        let usuarioNuevo=tipoUsuario;
+
         res.redirect("/")
     }
 }
