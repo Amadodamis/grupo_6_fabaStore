8b7879ca0f03d4754837df08771c387ce2c241fb
franciscocgce <franciscocgce@hotmail.com.ar>
2021-12-25 15:22:52 -0300

    edición de productos con Base de Datos

--- controllers/editProductsController.js
+++ controllers/editProductsController.js
@@ -1,23 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+//requiero la base de datos
+const db = require('../database/models/');
+
 /* En la constante "productos" ya tienen los productos que están JSON */
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let controller = {
     edit:(req,res)=>{
-        let id=req.params.id;
+        /*let id=req.params.id;
         const producto = productos.find(producto =>{
             return producto.id == id
         })
         res.render("editProduct",{
             producto: producto
-        })
+        })*/
+        db.Product.findByPk(req.params.id)
+            .then(producto => {
+                res.render('EditProduct.ejs', {producto:producto});
+            });
     },
     update:(req,res)=>{
         //Editamos el producto que va a llegar por parámetro (su ID)
-        let id = req.params.id;
+        /*let id = req.params.id;
         let productToEdit = productos.find(product => {
             return product.id == id
         })
@@ -47,7 +54,32 @@ let controller = {
         });
 
         fs.writeFileSync(productsFilePath, JSON.stringify(productos, null , " "))
-        res.redirect("/")
+        res.redirect("/")*/
+        let productToEdit = db.Product.findByPk(req.params.id);
+        db.Product.update(
+            {
+                modelo: req.body.producto,
+                marca: req.body.marca,
+                tipoProducto: req.body.tipo,
+                img: req.file ? req.file.filename : productToEdit.img,
+                precio: req.body.precio[0],
+                stock: req.body.stock[0],
+                stockCant: req.body.stock[1],
+                oferta: req.body.stock[2],
+                ofertaPorcentaje: req.body.descuento,
+                precioConOferta: req.body.precio[1],
+                cuotasSinInteres: req.body.cuotas[0],
+                cantCuotas: req.body.cuotas[1],
+                interescuota: req.body.cuotas[2],
+                precioEnCuotas: req.body.cuotas[3],
+                especificaciones: req.body.cuotas[4]
+            },
+            {
+             where: {id: req.params.id}
+            })
+            .then (() => {
+                res.redirect("/")
+               })
     },
     delete:(req,res)=>{
         // Eliminamos el producto que llegó por parametro su ID
--- controllers/uploadProductsController.js
+++ controllers/uploadProductsController.js
@@ -7,6 +7,10 @@ const multer = require ("multer")
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+//requiero la base de datos
+const db = require('../database/models/');
+
+//requiero las funcionalidades de productos
 const funcionesProductos=require("../views/source/funcionesProductos")
 
 let tipoProducto={
@@ -56,7 +60,34 @@ let controller = {
         fs.writeFileSync(productsFilePath,productosJson)
 
         res.redirect("/")
-    },
+    }
+    /*formulario: (req,res)=>{
+        db.Product.create({
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+            length: req.body.length,
+            id: req.body.id,
+            modelo:req.body.modelo,
+            marca: req.body.marca,
+            tipoProducto:req.body.tipoProducto,
+            img:req.file.img,
+            precio:req.body.precio,
+            stock:req.body.stock,
+            stockCant:req.body.stockCant,
+            oferta:req.body.oferta,
+            ofertaPorcentaje:req.body.ofertaPorcentaje,
+            precioConOferta:req.body.precioConOferta,
+            cuotasSinInteres: req.body.cuotasSinInteres,
+            cantCuotas: req.body.cantCuotas,
+            interescuota: req.body.interescuota,
+            precioEnCuotas: req.body.precioEnCuotas,
+            especificaciones: req.body.especificaciones
+           })
+           .then (() => {
+            res.redirect("/products")
+           })*/
 }
 
 module.exports = controller;
