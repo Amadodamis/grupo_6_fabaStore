4ca65a80de285a620fa0b52a3a6309bf570368da
Amado <amadodamis@gmail.com>
2022-02-07 17:04:15 -0300

    paginado de los productos

--- wwwfaba-react-proyecto-omtegrador/src/components/partials/partialsdashboard/articleBotListadoProductos.jsx
+++ wwwfaba-react-proyecto-omtegrador/src/components/partials/partialsdashboard/articleBotListadoProductos.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import '../../../css/stylesDashboard.css';
 import '../../../css/styles.css';
 import TablePartial from "./tablePartial";
 
+const productosPorPagina=10;
 function ArticleBotListadoProductos(props){
-    
+
+    const [productos,setProductos] = useState(props.productos)
+    const [items,setItems]= useState([...props.productos].splice(0,productosPorPagina))
+    const [currentPage,setCurrentPage]=useState(0)
+
+    const  nextHandler =()=>{
+        const totalElementos= productos.length;
+
+        const nextPage=currentPage +1;
+        const firstIndex=nextPage *productosPorPagina
+
+        if (firstIndex >= totalElementos) {
+
+            return;
+        }
+
+        setItems([...productos].splice(firstIndex,productosPorPagina))
+        setCurrentPage(nextPage)
+
+     }
+
+
+   const  prevHandler =()=>{
+        const prevPage=currentPage -1
+
+        if (prevPage < 0 ) return;
+
+        const firstIndex = prevPage *productosPorPagina
+        setItems([...productos].splice(firstIndex,productosPorPagina))
+        setCurrentPage(prevPage)
+   }
+
+
 return(
 
     <React.Fragment>
@@ -15,7 +49,13 @@ return(
                             <p className="parrafo-listado-productos">cargando... </p> 
 
                             }
-                            {props.productos.length > 0 && <TablePartial productos= {props.productos}/> }
+                            {props.productos.length > 0 && 
+                            <TablePartial 
+                            currentPage={currentPage}
+                            productos= {items}
+                            prevHandler={prevHandler}
+                            nextHandler={nextHandler}
+                            /> }
             </article> 
     </React.Fragment>
 )} 
--- wwwfaba-react-proyecto-omtegrador/src/components/partials/partialsdashboard/tablePartial.jsx
+++ wwwfaba-react-proyecto-omtegrador/src/components/partials/partialsdashboard/tablePartial.jsx
@@ -4,28 +4,37 @@ import '../../../css/stylesDashboard.css';
 import '../../../css/styles.css';
 
 function TablePartial(props){
+
+const productosList=props.productos.map((prod,i)=>{
+    return (
+        <tr>
+           <td>{prod.id}</td>
+           <td>{prod.marca.nombreMarca}</td>
+           <td>{prod.modelo}</td>
+           <td>{prod.precio}$</td>
+           <td>{prod.stockCantidad}</td>
+           <td>{prod.ofertaPorcentaje}</td>
+           </tr>
+       
+    )
+})
+
+
 return(
 
     <React.Fragment>
+        <h1 className="Pagina-dashboard-lista-productos">Pagina {props.currentPage} </h1>
+        
+
         <thead className="indice-tabla">
                 <tr>
                     <th>ID </th><th>Marca </th> <th>MODELO </th> <th>PRECIO</th> <th>STOCK</th> <th>% OFERTA</th>
                 </tr>
         </thead>
-            {props.productos.map((prod,i)=>{
-                                return (
-                                    <tr>
-                                       <td>{prod.id}</td>
-                                       <td>{prod.marca.nombreMarca}</td>
-                                       <td>{prod.modelo}</td>
-                                       <td>{prod.precio}$</td>
-                                       <td>{prod.stockCantidad}</td>
-                                       <td>{prod.ofertaPorcentaje}</td>
-                                       </tr>
-                                   
-                                )
-                        })}
 
+        {productosList}
+        <button onClick={props.prevHandler}>prev</button>
+        <button onClick={props.nextHandler}>next</button>
     </React.Fragment>
 )} 
 export default TablePartial
